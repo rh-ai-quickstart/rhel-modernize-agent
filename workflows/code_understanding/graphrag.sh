7a87d94a57b7adfaa775b63c618f689beaebf72f
Michael Dawson <midawson@redhat.com>
2026-07-02 17:23:35 -0400

    fix: adjust for graphrag 2.6 ?
    Signed-off-by: Michael Dawson <midawson@redhat.com>

--- workflows/code_understanding/graphrag.sh
+++ workflows/code_understanding/graphrag.sh
@@ -1,9 +1,6 @@
 echo "Initializing GraphRAG index..."
 python -m graphrag init --force --root $1 2>&1
 
-echo "Removing default graphrag prompts to ensure custom prompts are used..."
-rm -f $1/prompts/extract_graph.txt
-
 echo "Copying settings.yaml..."
 cp templates/settings.yaml $1/settings.yaml
 sleep 5
